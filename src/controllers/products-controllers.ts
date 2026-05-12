cd74d5d75b2747e7b72207d6a420b6822423da01
João Paulo Freitas Pinho <joaopaulofreitasp23@gmail.com>
2026-05-12 12:37:25 -0300

    feat: add error handling middleware and AppError class

--- src/controllers/products-controllers.ts
+++ src/controllers/products-controllers.ts
@@ -1,9 +1,9 @@
 import { NextFunction, Request, Response } from "express";
+import { AppError } from "@/utils/app-error";
 
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
-
       return response.json({ message: "Hello World!" });
     } catch (error) {
       next(error);
