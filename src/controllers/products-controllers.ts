6355695a23f9d023dde858866b279ce6744050d8
João Paulo Freitas Pinho <joaopaulofreitasp23@gmail.com>
2026-05-12 15:08:18 -0300

    feat: add Zod validation to product creation and enhance error handling

--- src/controllers/products-controllers.ts
+++ src/controllers/products-controllers.ts
@@ -1,10 +1,24 @@
 import { NextFunction, Request, Response } from "express";
-import { AppError } from "@/utils/app-error";
-
+import { z } from "zod";
 class ProductController {
   async index(request: Request, response: Response, next: NextFunction) {
     try {
       return response.json({ message: "Hello World!" });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async create(request: Request, response: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({ 
+        name: z.string().trim().min(6),
+        price: z.number().gt(0),
+       });
+
+      const { name, price } = bodySchema.parse(request.body);
+
+      return response.status(201).json({ name, price });
     } catch (error) {
       next(error);
     }
